6d40492c463dddb26ba00176cea2db3b2712af58
ericericericjin <jyseric@126.com>
2023-08-06 08:16:57 +0800

    Added CAN bus fric motor support

--- application/gimbal/shoot_task.c
+++ application/gimbal/shoot_task.c
@@ -38,12 +38,22 @@ struct pid_param turn_motor_param =
         .integral_limit = 10000,
 };
 
+struct pid_param fric_motor_param = 
+{
+    .p = 10.0f,
+    .i = 0.3f,
+    .max_out = 30000,
+    .integral_limit = 10000,
+};
+
 static void shoot_dr16_data_update(uint32_t eventID, void *pMsgData, uint32_t timeStamp);
 
 struct shoot shoot;
 struct rc_device shoot_rc;
 
 int32_t shoot_firction_toggle(shoot_t p_shoot);
+int32_t shoot_firction_on(shoot_t p_shoot);
+int32_t shoot_firction_off(shoot_t p_shoot);
 
 void shoot_task(void const *argument)
 {
@@ -60,6 +70,9 @@ void shoot_task(void const *argument)
 
     shoot_pid_init(&shoot, "Shoot", turn_motor_param, DEVICE_CAN2, 0x207);
 
+    uint16_t fric_can_id[2] = {0x201, 0x202};
+    shoot_fric_wheel_init(&shoot, "CANFRIC", fric_motor_param, DEVICE_CAN2, fric_can_id, 16384);
+
     int is_shooting = 0;
 
     while (1)
@@ -67,14 +80,19 @@ void shoot_task(void const *argument)
         /* dr16 data update */
         EventMsgProcess(&listSubs, 0);
 
-        if (rc_device_get_state(&shoot_rc, RC_S1_MID2UP) == E_OK)
-        {
-            shoot_firction_toggle(&shoot);
-        }
-        else if (rc_device_get_state(&shoot_rc, RC_S1_UP2MID) == E_OK)
-        {
-            shoot_set_cmd(&shoot, SHOOT_STOP_CMD, 0);
-        }
+        // if (rc_device_get_state(&shoot_rc, RC_S1_MID2UP) == E_OK)
+        // {
+        //     shoot_firction_toggle(&shoot);
+        // }
+        // else if (rc_device_get_state(&shoot_rc, RC_S1_UP2MID) == E_OK)
+        // {
+        //     shoot_set_cmd(&shoot, SHOOT_STOP_CMD, 0);
+        // }
+
+        if (rc_device_get_state(&shoot_rc, RC_S1_UP) == E_OK)
+            shoot_firction_on(&shoot);
+        else
+            shoot_firction_off(&shoot);
 
         // if (rc_device_get_state(&shoot_rc, RC_S1_MID2DOWN) == E_OK)
         // {
@@ -86,16 +104,17 @@ void shoot_task(void const *argument)
         int16_t wheel = shoot_rc.rc_info.wheel;
         // log_i("wheel value: %d.", wheel);
 
-        if (wheel > 330 && !is_shooting)
+        if (shoot_rc.rc_info.mouse.l || wheel > 330 && !is_shooting)
         {
             shoot_set_cmd(&shoot, SHOOT_CONTINUOUS_CMD, 0);
             is_shooting = 1;
         }
-        else if (wheel < 330 && is_shooting)
+        else if ((shoot_rc.rc_info.mouse.l == 0) && wheel < 330 && is_shooting)
         {
             shoot_set_cmd(&shoot, SHOOT_ONCE_CMD, 0);
             is_shooting = 0;
         }
+        fric_pid_calc(&shoot);
 
         // if (rc_device_get_state(&shoot_rc, RC_S2_DOWN) != E_OK)
         // {
@@ -124,6 +143,7 @@ int32_t shoot_firction_toggle(shoot_t p_shoot)
         shoot_set_fric_speed(p_shoot, 1000, 1000);
         // Turn off laser
         __HAL_TIM_SetCompare(&htim3, TIM_CHANNEL_3, 0);
+        turn_off_can_fric(&shoot);
     }
     else
     {
@@ -136,9 +156,42 @@ int32_t shoot_firction_toggle(shoot_t p_shoot)
 
         // Turn on laser
         __HAL_TIM_SetCompare(&htim3, TIM_CHANNEL_3, 1999);
+
+        turn_on_can_fric(&shoot);
     }
     toggle = ~toggle;
     return 0;
+}
+
+int32_t shoot_firction_on(shoot_t p_shoot)
+{
+    // PWM
+    if (get_speed_cfg() == NOJMP_SPEED)
+        shoot_set_fric_speed(p_shoot, FIRC_MAX_SPEED, FIRC_MAX_SPEED);
+    else 
+        shoot_set_fric_speed(p_shoot, FIRC_MAX_SPEED_PLUG, FIRC_MAX_SPEED_PLUG);
+    // Turn on laser
+    __HAL_TIM_SetCompare(&htim3, TIM_CHANNEL_3, 1999);
+
+    // CAN
+    turn_on_can_fric(&shoot);
+
+    // Flag
+    static uint8_t toggle = 1;   
+
+    return E_OK;
+}
+
+int32_t shoot_firction_off(shoot_t p_shoot)
+{
+    shoot_set_fric_speed(p_shoot, 1000, 1000);
+    // Turn off laser
+    __HAL_TIM_SetCompare(&htim3, TIM_CHANNEL_3, 0);
+    turn_off_can_fric(&shoot);
+
+    static uint8_t toggle = 0;
+    
+    return E_OK;
 }
 
 struct shoot *get_shoot(void)
